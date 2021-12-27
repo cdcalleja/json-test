639431ce55f76fb994b08613d8d5af0df19e50dd
cdcalleja <christiandcalleja@yahoo.com.ar>
2021-12-27 14:26:53 -0300

    1 file modified, 1 added

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ var JSONObj = {};
 var JSONObj = new Object();
 
 var JSONObj = { nombrelibro: 'El hacedor', precio: 500 };
+
+const deportista = {
+  nombre: 'Messi',
+  champions: 4,
+};
+
+console.log(Json.stringify(deportista));
+
+fetch ('http://127.0.0.1:5500/prueba.Json')
+.then((response) => response.json())
+.then ((data) => console.log(data));
+
